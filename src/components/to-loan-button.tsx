ea60439828550022a643f14617b671a925ed4d67
Lucas de Paula <lucasdafonsecadepaula@gmail.com>
2024-10-23 09:29:51 -0300

    Fixing bug where modal dialog close when upload a image

--- src/components/to-loan-button.tsx
+++ src/components/to-loan-button.tsx
@@ -9,14 +9,6 @@ import {
   LoaderPinwheel,
 } from 'lucide-react'
 import { Button } from '@/components/ui/button'
-import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-} from '@/components/ui/dialog'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
 import { Textarea } from '@/components/ui/textarea'
@@ -33,7 +25,7 @@ import { BUCKET_NAME } from '@/config'
 export function ToLoanButton() {
   const supabase = createSupabaseClient()
   const fileInputRef = useRef<HTMLInputElement>(null)
-  const [step, setStep] = useState(1)
+  const [step, setStep] = useState(0)
   const [copied, setIsCopied] = useState(false)
   const [newProduct, setNewProduct] = useState({
     name: '',
@@ -110,158 +102,163 @@ export function ToLoanButton() {
   }
 
   return (
-    <Dialog>
-      <DialogTrigger asChild>
-        <Button className="w-full">
+    <div className="">
+      {step === 0 ? (
+        <Button className="w-full" onClick={() => setStep(1)}>
           <Plus className="mr-2 h-4 w-4" />
           Emprestar
         </Button>
-      </DialogTrigger>
-      <DialogContent>
-        {step === 1 ? (
-          <>
-            <DialogHeader>
-              <DialogTitle>Dados do Empréstimo</DialogTitle>
-            </DialogHeader>
-            <form onSubmit={handleCreateProduct} className="space-y-4">
-              <div>
-                <Label htmlFor="image">Foto</Label>
-                <div className="mt-1 flex items-center space-x-4">
-                  {newProduct.image ? (
-                    <Image
-                      src={newProduct.image}
-                      alt="Preview"
-                      width={100}
-                      height={100}
-                      className="rounded-md"
-                    />
-                  ) : (
-                    <div
-                      onClick={handleAddImageClick}
-                      className="w-24 h-24 border-2 border-dashed border-gray-300 rounded-md flex items-center justify-center"
-                    >
-                      <Upload className="w-8 h-8 text-gray-400" />
-                    </div>
-                  )}
-                  <Input
-                    ref={fileInputRef}
-                    id="image"
-                    type="file"
-                    multiple={false}
-                    accept="image/*"
-                    onChange={handleImageChange}
-                    className="hidden"
+      ) : null}
+
+      {step === 1 ? (
+        <>
+          <div>
+            <h4 className="text-lg pb-8">Dados do Empréstimo</h4>
+          </div>
+          <form onSubmit={handleCreateProduct} className="space-y-4">
+            <div>
+              <Label htmlFor="image">Foto</Label>
+              <div className="mt-1 flex items-center space-x-4">
+                {newProduct.image ? (
+                  <Image
+                    src={newProduct.image}
+                    alt="Preview"
+                    width={100}
+                    height={100}
+                    className="rounded-md"
                   />
+                ) : (
                   <div
                     onClick={handleAddImageClick}
-                    className="w-full h-24 text-sm sm:text-base p-4 border-2 border-dashed border-gray-300 rounded-md flex sm:flex-col gap-2 items-center justify-center text-gray-400"
+                    className="w-24 h-24 border-2 border-dashed border-gray-300 rounded-md flex items-center justify-center"
                   >
-                    <ImagePlus className="w-8 h-8 text-gray-400" />
-                    Clique aqui para adicionar uma foto
+                    <Upload className="w-8 h-8 text-gray-400" />
                   </div>
+                )}
+                <Input
+                  ref={fileInputRef}
+                  id="image"
+                  type="file"
+                  multiple={false}
+                  accept="image/*"
+                  onChange={handleImageChange}
+                  className="hidden"
+                />
+                <div
+                  onClick={handleAddImageClick}
+                  className="w-full h-24 text-sm sm:text-base p-4 border-2 border-dashed border-gray-300 rounded-md flex sm:flex-col gap-2 items-center justify-center text-gray-400"
+                >
+                  <ImagePlus className="w-8 h-8 text-gray-400" />
+                  Clique aqui para adicionar uma foto
                 </div>
               </div>
-              <div>
-                <Label htmlFor="name">Nome</Label>
-                <Input
-                  id="name"
-                  placeholder="Ex: Pote, panela ou livro..."
-                  value={newProduct.name}
-                  onChange={(e) =>
-                    setNewProduct({ ...newProduct, name: e.target.value })
-                  }
-                  required
-                />
-              </div>
-              <div>
-                <Label htmlFor="description">Descrição</Label>
-                <Textarea
-                  id="description"
-                  placeholder="Coloque uma descrição do item e o nome da pessoa que você vai emprestar."
-                  value={newProduct.description}
-                  onChange={(e) =>
-                    setNewProduct({
-                      ...newProduct,
-                      description: e.target.value,
-                    })
-                  }
-                  required
-                />
-              </div>
-              <div className="flex items-center space-x-2">
-                <Checkbox
-                  id="isPublic"
-                  checked={newProduct.isPublic}
-                  onCheckedChange={(checked) =>
-                    setNewProduct({
-                      ...newProduct,
-                      isPublic: checked as boolean,
-                    })
-                  }
-                />
-                <Label htmlFor="isPublic">
-                  Aceito eternizar o produto em um mural, caso o item não seja
-                  devolvido
-                </Label>
-              </div>
-              <div className="w-full flex justify-end">
-                <Button className="ml-auto" type="submit" disabled={isPending}>
-                  {isPending ? (
-                    <>
-                      <LoaderPinwheel className="animate-spin" />
-                      Criando empréstimo
-                    </>
-                  ) : (
-                    'Criar empréstimo'
-                  )}
-                </Button>
-              </div>
-            </form>
-          </>
-        ) : null}
-
-        {step === 2 ? (
-          <>
-            <DialogHeader className="text-left">
-              <DialogTitle>
-                Compartilhe o link para quem pegou emprestado
-              </DialogTitle>
-              <DialogDescription>
-                Último passo, compartilhe o link abaixo apenas com a pessoa que
-                pegou emprestado.
-              </DialogDescription>
-            </DialogHeader>
-            <div className="space-y-4">
-              <div className="flex items-center space-x-2 w-full">
-                <Input value={sharedLink} readOnly />
-                <Button
-                  size="icon"
-                  variant="outline"
-                  onClick={() => copyToClipboard(sharedLink)}
-                >
-                  {copied ? (
-                    <Check className="h-4 w-4" />
-                  ) : (
-                    <Copy className="h-4 w-4" />
-                  )}
-                  <span className="sr-only">Copy link</span>
-                </Button>
-              </div>
-              <div className="flex flex-wrap sm:justify-center gap-2 sm:gap-4">
-                <Button onClick={handleShareWithWhatsapp} variant="outline">
-                  <Image
-                    src={whatsappIcon}
-                    alt="whatsapp icon"
-                    height={16}
-                    width={16}
-                  />
-                  Compartilhe com Whatsapp
-                </Button>
-              </div>
             </div>
-          </>
-        ) : null}
-      </DialogContent>
-    </Dialog>
+            <div>
+              <Label htmlFor="name">Nome</Label>
+              <Input
+                id="name"
+                placeholder="Ex: Pote, panela ou livro..."
+                value={newProduct.name}
+                onChange={(e) =>
+                  setNewProduct({ ...newProduct, name: e.target.value })
+                }
+                required
+              />
+            </div>
+            <div>
+              <Label htmlFor="description">Descrição</Label>
+              <Textarea
+                id="description"
+                placeholder="Coloque uma descrição do item e o nome da pessoa que você vai emprestar."
+                value={newProduct.description}
+                onChange={(e) =>
+                  setNewProduct({
+                    ...newProduct,
+                    description: e.target.value,
+                  })
+                }
+                required
+              />
+            </div>
+            <div className="flex items-center space-x-2">
+              <Checkbox
+                id="isPublic"
+                checked={newProduct.isPublic}
+                onCheckedChange={(checked) =>
+                  setNewProduct({
+                    ...newProduct,
+                    isPublic: checked as boolean,
+                  })
+                }
+              />
+              <Label htmlFor="isPublic">
+                Aceito eternizar o produto em um mural, caso o item não seja
+                devolvido
+              </Label>
+            </div>
+            <div className="w-full flex gap-4 justify-end">
+              <Button
+                variant="outline"
+                className=""
+                disabled={isPending}
+                onClick={() => setStep(0)}
+              >
+                Voltar
+              </Button>
+              <Button className="" type="submit" disabled={isPending}>
+                {isPending ? (
+                  <>
+                    <LoaderPinwheel className="animate-spin" />
+                    Criando empréstimo
+                  </>
+                ) : (
+                  'Criar empréstimo'
+                )}
+              </Button>
+            </div>
+          </form>
+        </>
+      ) : null}
+
+      {step === 2 ? (
+        <>
+          <div className="text-left">
+            <h4>Compartilhe o link para quem pegou emprestado</h4>
+            <p>
+              Último passo, compartilhe o link abaixo apenas com a pessoa que
+              pegou emprestado.
+            </p>
+          </div>
+          <div className="space-y-4">
+            <div className="flex items-center space-x-2 w-full">
+              <Input value={sharedLink} readOnly />
+              <Button
+                size="icon"
+                variant="outline"
+                onClick={() => copyToClipboard(sharedLink)}
+              >
+                {copied ? (
+                  <Check className="h-4 w-4" />
+                ) : (
+                  <Copy className="h-4 w-4" />
+                )}
+                <span className="sr-only">Copy link</span>
+              </Button>
+            </div>
+            <div className="flex flex-wrap sm:justify-center gap-2 sm:gap-4">
+              <Button onClick={handleShareWithWhatsapp} variant="outline">
+                <Image
+                  src={whatsappIcon}
+                  alt="whatsapp icon"
+                  height={16}
+                  width={16}
+                />
+                Compartilhe com Whatsapp
+              </Button>
+            </div>
+          </div>
+        </>
+      ) : null}
+    </div>
   )
 }
