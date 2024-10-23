29c86bf30d77bb6bbb6e1805a62273e3ee51e618
Lucas de Paula <lucasdafonsecadepaula@gmail.com>
2024-10-23 10:49:04 -0300

    Realizing theres no fix...

---
+++ src/app/emprestar/page.tsx
@@ -0,0 +1,31 @@
+import NotificationPermissionCard from '@/components/notification-permission-card'
+import { ToLoanComponent } from '@/components/to-loan-component'
+import { TopBar } from '@/components/top-bar'
+import { createSupabaseServer } from '@/lib/supabase/server'
+import { redirect } from 'next/navigation'
+
+export default async function Page() {
+  const supabase = createSupabaseServer()
+
+  const {
+    data: { user },
+  } = await supabase.auth.getUser()
+
+  if (!user) {
+    return redirect('/')
+  }
+
+  return (
+    <div className="flex flex-col min-h-screen">
+      <TopBar />
+      <main>
+        <div className="min-h-screen bg-background p-4 md:p-6">
+          <div className="max-w-4xl mx-auto space-y-4 pt-16">
+            <ToLoanComponent />
+          </div>
+        </div>
+      </main>
+      <NotificationPermissionCard />
+    </div>
+  )
+}
